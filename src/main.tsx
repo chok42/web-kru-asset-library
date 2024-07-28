ad59c044b2be63fbeb1d574048bca9217ed7a9d8
Sathaporn Bunkoed <sathaporn.chok42@gmail.com>
2024-07-28 17:38:27 +0700

    base name

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router
-    basename='/'
+    basename='/kru'
     >
       <App />
     </Router>
